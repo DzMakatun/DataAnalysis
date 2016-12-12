e30f30622fba514e76ce8ccad7e78629b7b9fb5e
Dzmitry Makatun <d.i.makatun@gmail.com>
2016-12-12 14:02:26 +0100

    first

---
+++ DataAnalysis/src/Test.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 12, 2016
+
+@author: Dima
+'''
+
+def hello(myInput):
+    return "Hello World: " + myInput
+ 
+myInput = input("Please type your name: ") 
+print(hello(myInput))
