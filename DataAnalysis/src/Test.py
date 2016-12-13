77004202c316f65b7e1c55a8ffbe46dda5f8dea0
Dzmitry Makatun <d.i.makatun@gmail.com>
2016-12-13 16:22:14 +0100

    implementation for 3 nodes

--- DataAnalysis/src/Test.py
+++ DataAnalysis/src/Test.py
@@ -1,11 +1,19 @@
-'''
-Created on Dec 12, 2016
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: Dima
-'''
+# example data
+x = [1,2,3]#np.arange(0.1, 4, 0.5)
+y = [2,4,6]# np.exp(-x)
+err = [0.1, 0.3, 0.3]
+# First illustrate basic pyplot interface, using defaults where possible.
+plt.figure()
+lines = plt.errorbar(x, y, yerr=err)
 
-def hello(myInput):
-    return "Hello World: " + myInput
- 
-myInput = input("Please type your name: ") 
-print(hello(myInput))
+# use keyword args
+plt.setp(lines[0], marker='o', color='r', linewidth=2.0)
+#plt.title("Title")
+plt.xlabel('Bandwidth (Gbps)')
+plt.ylabel('Makespan improvement (%)')
+plt.axis([0, 4, 0, 7])
+plt.show()
